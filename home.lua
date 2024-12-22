13e058292d1174839943723a5177295ffb000d2e
piconet-picotron <piconet.picotron@gmail.com>
2024-12-21 22:14:19 -0700

    Add files via upload

---
+++ home.lua
@@ -0,0 +1,12 @@
+--[[pod_format="raw",created="2024-03-20 06:16:57",modified="2024-12-22 05:12:53",revision=3630]]
+
+title = "home"
+_init = function(self, explrorer)
+	clr = 2
+end
+_draw = function(self, explorer)
+	cls(clr or 7)
+end
+_update = function(self, explorer)
+
+end
