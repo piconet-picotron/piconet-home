9e164b138307212b8e6091bb761dd356623601fb
piconet-picotron <piconet.picotron@gmail.com>
2025-02-19 14:29:36 -0700

    Create 404.lua

---
+++ 404.lua
@@ -0,0 +1,13 @@
+title = "404 Error"
+_init = function(self, explorer)
+	
+end
+_draw = function(self, explorer)
+	cls(0)
+	print([[
+	404! This is a placeholder for a custom 404 page :)
+	]])
+end
+_update = function(self, explorer)
+
+end
